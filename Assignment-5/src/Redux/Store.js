18b57a4b7b67c3dd431a28f8b9fedd26a67f7eaa
Himanshu Parida <137590331+Himanshu6623@users.noreply.github.com>
2024-11-03 10:48:25 +0530

    Delete Assignment-5 directory

--- Assignment-5/src/Redux/Store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import NewProduct from "./CounterSlice/Newproduct"
-import Review from "./CounterSlice/Review"
-export const store=configureStore({
-    reducer :{
-        new :NewProduct,
-        review : Review
-    }
-})
