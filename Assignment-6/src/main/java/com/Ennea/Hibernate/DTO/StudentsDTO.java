f33249ad22f24c92442645890f462e31ea2fc6c1
Himanshu Parida <137590331+Himanshu6623@users.noreply.github.com>
2024-11-17 09:14:23 +0530

    Update StudentsDTO.java

--- Assignment-6/src/main/java/com/Ennea/Hibernate/DTO/StudentsDTO.java
+++ Assignment-6/src/main/java/com/Ennea/Hibernate/DTO/StudentsDTO.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.Ennea.Hibernate.DTO;
 
 import com.Ennea.Hibernate.Entity.Courses;
@@ -20,26 +19,3 @@ public class StudentsDTO {
     private String name;
 
 }
-=======
-package com.Ennea.Hibernate.DTO;
-
-import com.Ennea.Hibernate.Entity.Courses;
-import jakarta.persistence.Column;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import java.util.HashMap;
-import java.util.List;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class StudentsDTO {
-
-    private String rollno;
-
-    private String name;
-
-}
->>>>>>> 2204a1e9195f87fabd0d8f466e69ae3d031c1e55
