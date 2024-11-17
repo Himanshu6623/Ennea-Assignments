4bd336e833b7ef63e5162a5fb07babe215bfc4db
Himanshu Parida <137590331+Himanshu6623@users.noreply.github.com>
2024-11-17 09:23:33 +0530

    Update StudentRepo.java

--- Assignment-6/src/main/java/com/Ennea/Hibernate/Repository/StudentRepo.java
+++ Assignment-6/src/main/java/com/Ennea/Hibernate/Repository/StudentRepo.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.Ennea.Hibernate.Repository;
 
 import com.Ennea.Hibernate.Entity.Students;
@@ -9,15 +8,4 @@ import org.springframework.stereotype.Repository;
 public interface StudentRepo extends JpaRepository<Students,String> {
     Students findByrollno(String rollno);
 }
-=======
-package com.Ennea.Hibernate.Repository;
 
-import com.Ennea.Hibernate.Entity.Students;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StudentRepo extends JpaRepository<Students,String> {
-    Students findByrollno(String rollno);
-}
->>>>>>> 2204a1e9195f87fabd0d8f466e69ae3d031c1e55
