88ffd06685241f8a713f90b6a26b5be04d81b69f
Himanshu Parida <137590331+Himanshu6623@users.noreply.github.com>
2024-11-17 09:27:32 +0530

    Update StudentService.java

--- Assignment-6/src/main/java/com/Ennea/Hibernate/Services/StudentServices/StudentService.java
+++ Assignment-6/src/main/java/com/Ennea/Hibernate/Services/StudentServices/StudentService.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.Ennea.Hibernate.Services.StudentServices;
 
 import com.Ennea.Hibernate.DTO.StudentsDTO;
@@ -50,57 +49,4 @@ public class StudentService {
 
         return "Invalid Username or Password";
     }
-=======
-package com.Ennea.Hibernate.Services.StudentServices;
-
-import com.Ennea.Hibernate.DTO.StudentsDTO;
-import com.Ennea.Hibernate.Mapper.StudentMapper;
-import com.Ennea.Hibernate.Entity.Students;
-import com.Ennea.Hibernate.Repository.StudentRepo;
-import com.Ennea.Hibernate.Services.JWTService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-
-@Service
-public class StudentService {
-    @Autowired
-    private StudentRepo studentRepo;
-
-    @Autowired
-    private StudentMapper studentMapper;
-
-    @Autowired
-    private JWTService jwt;
-
-    @Autowired
-    private AuthenticationManager authenticationManager;
-
-    public List<StudentsDTO> getallusers() {
-        List<Students> users = studentRepo.findAll();
-        return studentMapper.toStudentDtos(users);
-    }
-
-    public StudentsDTO AddUser(StudentsDTO studentsDTO) {
-        Students user = studentMapper.toStudentEntity(studentsDTO);
-        Students userResponse = studentRepo.save(user);
-        StudentsDTO response = studentMapper.toStudentDTO(userResponse);
-        return response;
-    }
-
-    public String verify(Students students) {
-        Authentication authentication =
-                authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(students.getRollno(), students.getPassword()));
-
-        if (authentication.isAuthenticated()) {
-            return jwt.getToken(students.getRollno());
-        }
-
-        return "Invalid Username or Password";
-    }
->>>>>>> 2204a1e9195f87fabd0d8f466e69ae3d031c1e55
 }
