4e5a259060310afc1674de33e0a02aef1e05eaad
Himanshu Parida <137590331+Himanshu6623@users.noreply.github.com>
2024-11-17 09:16:57 +0530

    Update CoursesSubscribed.java

--- Assignment-6/src/main/java/com/Ennea/Hibernate/Entity/CoursesSubscribed.java
+++ Assignment-6/src/main/java/com/Ennea/Hibernate/Entity/CoursesSubscribed.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.Ennea.Hibernate.Entity;
 
 
@@ -32,38 +31,4 @@ public class CoursesSubscribed {
 
     private Date subscriptionDate = new Date();
 }
-=======
-package com.Ennea.Hibernate.Entity;
 
-
-import com.Ennea.Hibernate.Entity.Courses;
-import com.Ennea.Hibernate.Entity.Students;
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import java.util.Date;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Entity
-@Table(name = "courses_subscribed")
-public class CoursesSubscribed {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "subscribedId")
-    private int subscribedId;
-
-    @ManyToOne
-    @JoinColumn(name = "rollno")
-    private Students student;
-
-    @ManyToOne
-    @JoinColumn(name = "courseId")
-    private Courses course;
-
-    private Date subscriptionDate = new Date();
-}
->>>>>>> 2204a1e9195f87fabd0d8f466e69ae3d031c1e55
