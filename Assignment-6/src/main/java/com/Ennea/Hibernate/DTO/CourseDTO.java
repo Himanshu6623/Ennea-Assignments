c8b76a27c9807394d56d92cd2daf920d2e355fd9
Himanshu Parida <137590331+Himanshu6623@users.noreply.github.com>
2024-11-17 09:15:02 +0530

    Update CourseDTO.java

--- Assignment-6/src/main/java/com/Ennea/Hibernate/DTO/CourseDTO.java
+++ Assignment-6/src/main/java/com/Ennea/Hibernate/DTO/CourseDTO.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.Ennea.Hibernate.DTO;
 
 import lombok.AllArgsConstructor;
@@ -18,22 +17,3 @@ public class CourseDTO {
 
     private String imageUrl;
 }
-=======
-package com.Ennea.Hibernate.DTO;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class CourseDTO {
-
-    private int CourseId;
-
-    private String Title;
-
-    private String Description;
-}
->>>>>>> 2204a1e9195f87fabd0d8f466e69ae3d031c1e55
