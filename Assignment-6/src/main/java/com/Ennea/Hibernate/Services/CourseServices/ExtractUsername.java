5e7199a68356c5175af56e530c70ad7b8a8b1e35
Himanshu Parida <137590331+Himanshu6623@users.noreply.github.com>
2024-11-17 09:26:52 +0530

    Update ExtractUsername.java

--- Assignment-6/src/main/java/com/Ennea/Hibernate/Services/CourseServices/ExtractUsername.java
+++ Assignment-6/src/main/java/com/Ennea/Hibernate/Services/CourseServices/ExtractUsername.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.Ennea.Hibernate.Services.CourseServices;
 
 import com.Ennea.Hibernate.Services.JWTService;
@@ -16,22 +15,3 @@ public class ExtractUsername {
         rollNo=rollno;
     }
 }
-=======
-package com.Ennea.Hibernate.Services.CourseServices;
-
-import com.Ennea.Hibernate.Services.JWTService;
-import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-@Component
-public class ExtractUsername {
-
-    public String rollNo="21311A6623";
-
-    public void Username(String rollno)
-    {
-        rollNo=rollno;
-    }
-}
->>>>>>> 2204a1e9195f87fabd0d8f466e69ae3d031c1e55
