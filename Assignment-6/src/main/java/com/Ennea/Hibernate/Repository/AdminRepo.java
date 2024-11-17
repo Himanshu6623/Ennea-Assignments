7a24d68129e62813b1f601c2e22c7b83627d76ba
Himanshu Parida <137590331+Himanshu6623@users.noreply.github.com>
2024-11-17 09:23:10 +0530

    Update AdminRepo.java

--- Assignment-6/src/main/java/com/Ennea/Hibernate/Repository/AdminRepo.java
+++ Assignment-6/src/main/java/com/Ennea/Hibernate/Repository/AdminRepo.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.Ennea.Hibernate.Repository;
 
 import com.Ennea.Hibernate.Entity.Admins;
@@ -9,15 +8,4 @@ import org.springframework.stereotype.Repository;
 public interface AdminRepo extends JpaRepository<Admins,Integer> {
     Admins findByEmail(String email);
 }
-=======
-package com.Ennea.Hibernate.Repository;
 
-import com.Ennea.Hibernate.Entity.Admins;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AdminRepo extends JpaRepository<Admins,Integer> {
-    Admins findByEmail(String email);
-}
->>>>>>> 2204a1e9195f87fabd0d8f466e69ae3d031c1e55
