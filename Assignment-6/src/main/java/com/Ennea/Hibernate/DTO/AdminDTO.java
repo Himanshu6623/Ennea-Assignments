78e0ad42f76fa5300994e0eef2e463a7d2bc98b8
Himanshu Parida <137590331+Himanshu6623@users.noreply.github.com>
2024-11-17 09:15:35 +0530

    Update AdminDTO.java

--- Assignment-6/src/main/java/com/Ennea/Hibernate/DTO/AdminDTO.java
+++ Assignment-6/src/main/java/com/Ennea/Hibernate/DTO/AdminDTO.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.Ennea.Hibernate.DTO;
 
 import lombok.AllArgsConstructor;
@@ -12,18 +11,3 @@ public class AdminDTO {
     private String name;
     private String email;
 }
-=======
-package com.Ennea.Hibernate.DTO;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class AdminDTO {
-    private String name;
-    private String email;
-}
->>>>>>> 2204a1e9195f87fabd0d8f466e69ae3d031c1e55
