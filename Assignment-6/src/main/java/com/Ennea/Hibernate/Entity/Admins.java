c20627886c19c9580dbfd8c86507dba12256eb73
Himanshu Parida <137590331+Himanshu6623@users.noreply.github.com>
2024-11-17 09:18:51 +0530

    Update Admins.java

--- Assignment-6/src/main/java/com/Ennea/Hibernate/Entity/Admins.java
+++ Assignment-6/src/main/java/com/Ennea/Hibernate/Entity/Admins.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.Ennea.Hibernate.Entity;
 
 import jakarta.persistence.Column;
@@ -35,41 +34,3 @@ public class Admins {
     private Date CreationDate;
 
 }
-=======
-package com.Ennea.Hibernate.Entity;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import java.util.Date;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Entity
-@Table
-public class Admins {
-    @Id
-    @GeneratedValue(
-            strategy = GenerationType.IDENTITY
-    )
-    @Column
-    private int id;
-    @Column
-    private String name;
-    @Column
-    private String email;
-    @Column
-    private String Password;
-    @Column
-    private Date CreationDate;
-
-}
->>>>>>> 2204a1e9195f87fabd0d8f466e69ae3d031c1e55
