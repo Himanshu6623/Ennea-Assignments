6b405f97864eadd205f7ebdc97b66f6661d055f3
Himanshu Parida <137590331+Himanshu6623@users.noreply.github.com>
2024-11-17 09:20:21 +0530

    Update JwtFilter.java

--- Assignment-6/src/main/java/com/Ennea/Hibernate/Filter/JwtFilter.java
+++ Assignment-6/src/main/java/com/Ennea/Hibernate/Filter/JwtFilter.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.Ennea.Hibernate.Filter;
 
 import com.Ennea.Hibernate.Services.AdminServices.myAdminDetailService;
@@ -63,69 +62,3 @@ public class JwtFilter extends OncePerRequestFilter {
         filterChain.doFilter(request,response);
     }
 }
-=======
-package com.Ennea.Hibernate.Filter;
-
-import com.Ennea.Hibernate.Services.AdminServices.myAdminDetailService;
-import com.Ennea.Hibernate.Services.JWTService;
-import com.Ennea.Hibernate.Services.StudentServices.myStudentDetailService;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
-import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
-
-import java.io.IOException;
-
-@Component
-public class JwtFilter extends OncePerRequestFilter {
-
-    @Autowired
-    private JWTService jwtService;
-
-    @Autowired
-    ApplicationContext context;
-
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String authHeader=request.getHeader("Authorization");
-        String token=null;
-        String username=null;
-
-        if(authHeader!=null && authHeader.startsWith("Bearer "))
-        {
-            token=authHeader.substring(7);
-            username=jwtService.extractUserName(token);
-        }
-
-        if(username!=null && SecurityContextHolder.getContext().getAuthentication()==null)
-        {
-            UserDetails userDetails = null;
-            try {
-                userDetails = context.getBean(myStudentDetailService.class).loadUserByUsername(username);
-            } catch (Exception e) {
-                try {
-                    userDetails = context.getBean(myAdminDetailService.class).loadUserByUsername(username);
-                } catch (Exception ignored) {
-                    System.out.println("User Not Found");
-                }
-            }
-            if(jwtService.validateToken(token, userDetails))
-            {
-                UsernamePasswordAuthenticationToken authToken=
-                        new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
-                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                SecurityContextHolder.getContext().setAuthentication(authToken);
-            }
-        }
-        filterChain.doFilter(request,response);
-    }
-}
->>>>>>> 2204a1e9195f87fabd0d8f466e69ae3d031c1e55
