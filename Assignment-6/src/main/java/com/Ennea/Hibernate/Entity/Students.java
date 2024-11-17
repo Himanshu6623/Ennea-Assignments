92d99e613cc1bb2f2202dbdc17c7a23bbe796041
Himanshu Parida <137590331+Himanshu6623@users.noreply.github.com>
2024-11-17 09:16:19 +0530

    Update Students.java

--- Assignment-6/src/main/java/com/Ennea/Hibernate/Entity/Students.java
+++ Assignment-6/src/main/java/com/Ennea/Hibernate/Entity/Students.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.Ennea.Hibernate.Entity;
 
 import io.micrometer.observation.Observation;
@@ -28,34 +27,3 @@ public class Students {
     private Date CreationDate;
 
 }
-=======
-package com.Ennea.Hibernate.Entity;
-
-import io.micrometer.observation.Observation;
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import java.util.Date;
-import java.util.*;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Entity
-@Table
-public class Students {
-    @Id
-    @Column(name="rollno")
-    private String rollno;
-    @Column
-    private String name;
-    @Column
-    private String Password;
-
-    @Column
-    private Date CreationDate;
-
-}
->>>>>>> 2204a1e9195f87fabd0d8f466e69ae3d031c1e55
