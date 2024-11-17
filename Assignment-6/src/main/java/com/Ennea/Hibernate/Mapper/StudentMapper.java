8018e235a91ff724d5db2e8f7476c66c573a70f9
Himanshu Parida <137590331+Himanshu6623@users.noreply.github.com>
2024-11-17 09:21:24 +0530

    Update StudentMapper.java

--- Assignment-6/src/main/java/com/Ennea/Hibernate/Mapper/StudentMapper.java
+++ Assignment-6/src/main/java/com/Ennea/Hibernate/Mapper/StudentMapper.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.Ennea.Hibernate.Mapper;
 import com.Ennea.Hibernate.DTO.StudentsDTO;
 import com.Ennea.Hibernate.Entity.Students;
@@ -18,25 +17,3 @@ public interface StudentMapper {
     List<StudentsDTO> toStudentDtos(List<Students> students);
 
 }
-=======
-package com.Ennea.Hibernate.Mapper;
-import com.Ennea.Hibernate.DTO.StudentsDTO;
-import com.Ennea.Hibernate.Entity.Students;
-import org.mapstruct.Mapper;
-import org.mapstruct.MappingConstants;
-
-import java.util.List;
-
-
-@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
-
-public interface StudentMapper {
-    StudentsDTO toStudentDTO(Students students);
-
-    Students toStudentEntity(StudentsDTO studentsDTO);
-
-    List<StudentsDTO> toStudentDtos(List<Students> students);
-
-    List<Students> toStudentEntities(List<StudentsDTO> studentsDTO);
-}
->>>>>>> 2204a1e9195f87fabd0d8f466e69ae3d031c1e55
